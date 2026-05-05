99ba1f777a972d06c26c400f10b765f762df087d
FaizanMohammed07 <aliffaizan1212@gmail.com>
2026-05-05 19:23:46 +0530

    added

--- apps/web/app/programs/master-classes/page.js
+++ apps/web/app/programs/master-classes/page.js
@@ -9,7 +9,7 @@ import ModulesSection from '@/components/ModulesSection';
 import HowItWorksSection from '@/components/HowItWorksSection';
 import OutcomesSection from '@/components/OutcomesSection';
 import MentorsSection from '@/components/MentorsSection';
-import TestimonialsSection from '@/components/TestimonialsSection';
+import FoundersTestimonialsSection from '@/components/FoundersTestimonialsSection';
 import FAQSection from '@/components/FAQSection';
 import DemoClassesSection from '@/components/DemoClassesSection';
 
@@ -313,7 +313,7 @@ export default function MasterClassesPage() {
       <CTAStripSection />
 
       {/* Testimonials Section */}
-      <TestimonialsSection />
+      <FoundersTestimonialsSection />
 
       {/* FAQ Section */}
       <FAQSection />
